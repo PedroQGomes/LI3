5ac52bc5a7ca01fffbaa7a5f31a3663d6ae0e671
zepsantos <josepgrs@gmail.com>
2019-05-03 16:14:09 +0100

    Projeto

---
+++ Java/src/com/grupo19/testeVendas.java
@@ -0,0 +1,46 @@
+package com.grupo19;
+import static java.lang.System.out;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class testeVendas {
+    public static void main(String[] args) {
+        List<String> lines;
+        Crono.start();
+        lines = lerLinhasWithBuff("Vendas_5M.txt");
+        out.println(Crono.stop());
+        out.println(lines.size());
+        out.println("SEGUNDO TESTE:");
+        Crono.start();
+        lerAllLines("Vendas_5M.txt");
+        out.println(Crono.stop());
+    }
+    public static List<String> lerLinhasWithBuff (String fichtxt) {
+        List<String> linhas = new ArrayList<>();
+        BufferedReader inFile;
+        String linha;
+        try {
+            inFile = new BufferedReader((new FileReader(fichtxt)));
+            while((linha = inFile.readLine()) != null) linhas.add(linha);
+        } catch(IOException exc) {
+            out.println(exc);
+
+        }
+        return linhas;
+    }
+
+    public static List<String> lerAllLines(String fichtxt) {
+        List<String> linhas = new ArrayList<>();
+        try {
+            linhas = Files.readAllLines(Paths.get(fichtxt));
+        } catch (IOException exc) {out.println(exc);}
+        return linhas;
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Trabalho de LI3
+Gestão de Vendas e Compras com Estruturas de dados para optimização com milhões de dados.
