dae99e48871a7c993fbc75f830f7c79e99249440
SLiFER999 <jogospg1999@gmail.com>
2019-05-30 17:36:40 +0100

    clientes e produtos
    class elementar

--- Java/src/com/grupo19/Product.java
+++ Java/src/com/grupo19/Product.java
@@ -1,4 +1,33 @@
 package com.grupo19;
 
 public class Product implements IProduct {
+    private String codigo;
+
+
+    public Product(){
+        this.codigo= "";
+    }
+
+    public Product(String a){
+        this.codigo= a;
+    }
+
+    public Product(Product x){
+        this.codigo = x.getCodigo();
+    }
+
+    public String getCodigo(){
+        return this.codigo;
+    }
+
+    public Product clone(){
+        return new Product(this);
+    }
+
+    public boolean equals(Object o){
+        if(this == o)return true;
+        if(o == null || this.getClass() != o.getClass())return false;
+        Product p = (Product) o;
+        return (this.codigo.equals(p));
+    }
 }
