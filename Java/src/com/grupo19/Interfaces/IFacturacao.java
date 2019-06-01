c6c34e4392655ca9c8e8fe8a356b4b776db8998d
alexrc1703 <alex.reis.costa@hotmail.com>
2019-06-01 17:51:42 +0100

    facturacao
    .

--- Java/src/com/grupo19/Interfaces/IFacturacao.java
+++ Java/src/com/grupo19/Interfaces/IFacturacao.java
@@ -1,4 +1,19 @@
 package com.grupo19.Interfaces;
 
 public interface IFacturacao {
+
+    void add(ISale sale);
+
+    IFacturacao initFacturacao(list<ISale>);
+    IFacturacao clone();
+    IFacturacao I_Facturacao();
+    IFacturacao (IFacturacao facturacao);
+    Map<int,collection<ISale>> totalProdMensal();
+    Map<int,collection<ISale>> totalFactMensal();
+    boolean contains(IFacturacao fact,);
+    Map<int,collection<ISale> > factPorMesClient(IClient client);
+    Double valorTotalFactMensal();
+    Double valorTotalProdMensal();
+
 }
+
