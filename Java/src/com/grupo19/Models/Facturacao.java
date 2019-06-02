0bc90395b8a48cfdba0bb502a778a7de7edbf717
alexrc1703 <alex.reis.costa@hotmail.com>
2019-06-02 18:40:00 +0100

    gete set atualizados
    falta testar

--- Java/src/com/grupo19/Models/Facturacao.java
+++ Java/src/com/grupo19/Models/Facturacao.java
@@ -5,114 +5,50 @@ import com.grupo19.Interfaces.IFacturacao;
 
 public class Facturacao implements IFacturacao, serializable {
 
-    /** colecao que associa mes(int) a uma list de sales com preco P */
-    private Map<int,List<Sale>> fatMensalP;
-
-    /** colecao que associa mes(int) a uma list de sales com preco P */
-    private Map<int,List<Sale>> fatMensalN;
-
-    /** list de sales com preco P*/
-    private List<Sale> salesListP;
-
-    /** list de sales com preco N*/
-    private List<Sale> salesListN;
-
+    /** list de maps de Isales */
+    private list<Map<String,FacturacaoPorProd>> arrayOfSales;
 
 
 
     /** construtor por omissao */
     public Facturacao(){
-        this.fatMensalP=getFatMensalP();
-        this.fatMensalN=getFatMensalN();
-        this.salesListP=getSaleListP();
-        this.salesListN=getSaleListN();
+        this.arrayOfSales=getArrayOfSales();
+
     }
 
     /** construtor de cópia */
     public Facturacao(Facturacao umaFacturacao){
-        this.fatMensalP=umaFacturacao.getFatMensalP();
-        this.fatMensalN=umaFacturacao.getFatMensalN();
-        this.salesListP=umaFacturacao.getSaleListP();
-        this.salesListN=umaFacturacao.getSaleListN();
+        this.arrayOfSales=umaFacturacao.getarrayOfSales();
     }
 
     /** construtor parametrizado */
-    public  Facturacao(Map<int,List<Sale>> mensalN, Map<int,List<Sale>> mensalP, List<Sale> salesN, List<Sale> salesP){
-        this.fatMensalP=mensalP;
-        this.fatMensalN=mensalN;
-        this.salesListP=salesP;
-        this.salesListN=salesN;
+    public  Facturacao(list<Map<String,FacturacaoPorProd>> arrayS){
+        this.arrayOfSales=arrayS;
     }
 
-    /** getter da colecao de sales com preco P*/
-    public Map<int, List<Sale>> getFatMensalP() {
-        return this.fatMensalP.entrySet()
-                .stream()
-                .collect(Collectors.toMap(e->e.getKey(), e->e.getValue().clone()));
-    }
 
-    /** setter da colecao de sales com preco P*/
-    public void setFatMensalP(Map<int, List<Sale>> fatMensalP) {
-        this.fatMensalP=fatMensalP.entrySet()
-                .stream()
-                .collect(Collectors.toMap(e->e.getKey(),e->e.getValue().clone()));
-    }
 
-    /** getter da colecao de sales com preco N*/
-    public Map<int, List<Sale>> getFatMensalN() {
-        return this.fatMensalN.entrySet()
-                .stream()
-                .collect(Collectors.toMap(e->e.getKey(), e->e.getValue().clone()));
-    }
-
-    /** setter da colecao de sales com preco N*/
-    public void setFatMensalN(Map<int, List<Sale>> fatMensalN) {
-        this.fatMensalN=fatMensalN.entrySet()
-                .stream()
-                .collect(Collectors.toMap(e->e.getKey(),e->e.getValue().clone()));
-    }
-
-    /** getter da list de sales P */
-    public List<Sale> getSalesListP(){
-        ArrayList novo = new ArrayList<Sale> (this.salesListP.size());
-        for ( Sale s : this.salesListP )
-        {
-            novo.add(s.clone());
+    /** getter da list de Isales*/
+    public List<Map<String,FacturacaoPorProd>> getArrayOfSales(){
+        ArrayList novo = new ArrayList<Map<String,FacturacaoPorProd>> (this.arrayOfSales.size());
+        for(Map mp:arrayOfSales){
+            novo.add(mp.clone());
         }
         return novo;
     }
 
     /** setter da list de sales P */
     @param list de sales com precos N e P
-    public void setSalesListP(List<Sale> salesAll){
-        this.salesListP= new ArrayList<List> (salesAll.size());
-        for ( Sale s: salesAll)
-        {
-            if(s.getSaleType().equals("P")) salesListP.add(s.clone());
-        }
-    }
-
-    /** getter da list de sales N*/
-    public List<Sale> getSalesListN(){
-        ArrayList novo = new ArrayList<Sale> (this.salesListN.size());
-        for ( Sale s : this.salesListN )
-        {
-            novo.add(s.clone());
+    public void setSArrayOfSales(List<Map<String,FacturacaoPorProd>> salesAll){
+        this.arrayOfSales= new ArrayList<Map<String,FacturacaoPorProd>> (salesAll.size());
+        for(Map mp:arrayOfSales){
+            novo.add(mp.clone());
         }
         return novo;
+
     }
 
-
-    /** setter da list de sales N*/
-    @param list de sales com precos N e P
-    public void setSalesListN(List<Sale> salesAll){
-        this.salesListN= new ArrayList<List> (salesAll.size());
-        for ( Sale s: salesAll)
-        {
-           if(s.getSaleType().equals("N")) salesListN.add(s.clone());
-        }
-    }
-
+    
     /** método clone */
     public boolean equals(Object obj){
         if(obj == this) {
@@ -121,42 +57,10 @@ public class Facturacao implements IFacturacao, serializable {
         if(obj == null || obj.getClass() != this.getClass()) {
             return false;
         }
-        Facturacao fact=(Facturacao) obj;
-        return fact.getFactMensalP().equals(fatMensalP) && fact.getFatMensalN().equals(fatMensalN)
-                && fact.getSalesListP().equals(salesListP) && fact.getSalesListN().equals(salesListN);
+        IFacturacao fact=(IFacturacao) obj;
+        return fact.getArrayOfSales().equals(arrayOfSales);
     }
 
 
 
-
-
-    /**  vendas de um determinado produto de um mes*/
-    public int totalSalesPerProductPerMonth(int month, Product prod){
-        int total=0;
-        list<Sale> lstP=new ArrayList<Sale>(fatMensalP.get(month));
-        list<Sale> lstN=new ArrayList<Sale>(fatMensalN.get(month));
-        total=lstP.stream().filter(s->s.getProduct().equals(prod)).sum();
-        total+=lstN.stream().filter(s->s.getProduct().equals(prod)).sum();
-        return  total;
-    }
-
-    /** valor total mensal*/
-    public double valorTotalFactMensal(int month){
-        int total=0;
-        list<Sale> lstP=new ArrayList<Sale>(fatMensalP.get(month));
-        list<Sale> lstN=new ArrayList<Sale>(fatMensalN.get(month));
-        total=lstP.stream().map(Sale::getPrice * Sale::getUnits).sum();
-        total=lstN.stream().map(Sale::getPrice * Sale::getUnits).sum();
-        return total;
-    }
-    /** top produtos mais comprados por um cliente por ordem decrescente */
-    public Map<Int,String> topProdPerClient( Client c);
-        for()
-
-
-
-
-
-
-    private list<Map<String,FacturacaoPorProd>> arrayOfSales;
 }
