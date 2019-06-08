6fde943e13a069868e39c073c9dc0b756ed2aa9e
alexrc1703 <alex.reis.costa@hotmail.com>
2019-06-08 22:50:36 +0100

    .

--- Java/src/com/grupo19/Interfaces/ICatProd.java
+++ Java/src/com/grupo19/Interfaces/ICatProd.java
@@ -6,22 +6,22 @@ public interface ICatProd {
 
     /**
      * adiciona um produto ao catalogo de produtos
-     * @param product
+     * @param product produto a inserir
      */
     void add(IProduct product);
 
 
     /**
      * dá a lista de n tamanho com os produtos mais vendidos
-     * @param n
-     * @return
+     * @param n numero de produtos a consultar
+     * @return lista de produtos
      */
     List<String> productsMostSell (int n);
 
 
     /**
      * verifica se um dado produto existe no catalogo de produtos
-     * @param codProd
+     * @param codProd string de produto
      * @return boolean
      */
     boolean contains(String codProd);
@@ -29,9 +29,9 @@ public interface ICatProd {
 
     /**
      * atualiza um produto que foi comprado numa dada filial numa certa quantidade
-     * @param codProd
-     * @param filial
-     * @param qnt
+     * @param codProd string de produto
+     * @param filial inteiro de filial
+     * @param qnt inteiro quantidade a inserir
      */
     void updateProductBought(String codProd , int filial , int qnt);
 
