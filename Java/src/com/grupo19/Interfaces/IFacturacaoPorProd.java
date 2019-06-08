16ac544cb0b91c45ac51707398816b387deaa73e
alexrc1703 <alex.reis.costa@hotmail.com>
2019-06-08 16:39:06 +0100

    com
    factpro

--- Java/src/com/grupo19/Interfaces/IFacturacaoPorProd.java
+++ Java/src/com/grupo19/Interfaces/IFacturacaoPorProd.java
@@ -4,11 +4,50 @@ import java.util.List;
 
 public interface IFacturacaoPorProd {
 
-
+    /**
+     *
+     * getter da list de sales
+     * @return  list de sales
+     *
+     */
     List<ISale> getSalesList();
+
+    /**
+     *
+     * adiciona uma ISale a list
+     * @param s sale a inserir
+     *
+     */
     void addSale(ISale s);
+
+    /**
+     *
+     * construtor de clone
+     * @return clone da IFacturacao
+     */
     IFacturacaoPorProd clone();
+
+
+    /**
+     * Devolve o total das sales do prod (qnt*preco)
+     * @return total
+     */
     double totalSaleProd();
+
+
+    /**
+     *
+     * Numero de diferentes clientes que realizaram compras
+     * @return double res
+     *
+     */
     double getDifClientsWhoBought();
+
+    /**
+     *
+     * Calcula a facturacao por filial
+     * @return  list por filial
+     *
+     */
     List<Double> factPerFilial();
 }
